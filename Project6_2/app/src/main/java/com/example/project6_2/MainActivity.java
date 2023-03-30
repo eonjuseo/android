1610664afe14f659b0364e08401d744ffb6e7f32
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-03-30 23:40:50 +0900

    실습 6-2 간단 웹브라우저 만들기

---
+++ Project6_2/app/src/main/java/com/example/project6_2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.project6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = edtUrl.getText().toString();
+                if (!address.startsWith("http://")) {
+                    address = "http://" + address;
+                }
+                webView.loadUrl(edtUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtUrl.setText(null);
+            }
+        });
+    }
+    class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+}
