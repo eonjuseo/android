46773b40e23e2df9970f9ce792f4136c2f45d298
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-04-14 02:02:07 +0900

    실습 10-1 새로운 액티비티 추가하기

---
+++ Project10_1/app/src/main/java/com/example/project10_1/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btnReturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        init();
+        initLr();
+    }
+
+    public void init(){
+        btnReturn = findViewById(R.id.btnReturn);
+    }
+
+    public void initLr(){
+        btnReturn.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
