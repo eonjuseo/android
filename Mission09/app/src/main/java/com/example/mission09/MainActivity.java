d783e5a6d772a94be4871bb190a984ab7b2b0f46
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-04-15 04:53:43 +0900

    도전09

---
+++ Mission09/app/src/main/java/com/example/mission09/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mission09;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CustomFragment fragment = new CustomFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.frame,fragment);
+        fragmentTransaction.commit();
+    }
+}
